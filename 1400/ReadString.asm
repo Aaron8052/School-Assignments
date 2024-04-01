3e325517662ca0c0299a0f8f6b81d688fb3f7896
FengYan枫炎 <68401855+Aaron8052@users.noreply.github.com>
2024-04-01 10:38:48 -0400

    Assembly Read String

---
+++ 1400/ReadString.asm
@@ -0,0 +1,29 @@
+.data
+prompt:		.asciiz "Please enter your name: "
+#char array
+name:		.space 31		#max of 30 char
+result:		.asciiz "Your name: "
+
+.text
+main:
+	li	$v0, 4		# output
+	la	$a0, prompt
+	syscall
+	
+	# cin.getline()
+	li	$v0, 8		# input string
+	la	$a0, name	# store input in the variable, name
+	li	$a1, 30		# max char from input
+	syscall
+	
+	li	$v0, 4		# output name
+	la	$a0, result
+	syscall
+	
+	# not necessary to set 4 again since line 18 is already set to 4
+	#li	$v0, 4		# output name
+	la	$a0, name
+	syscall
+	
+	li	$v0, 10
+	syscall			#exit
