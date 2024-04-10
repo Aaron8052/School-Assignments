220897527d564a9d43977d0ecc3ed3ca636f80b8
FengYan枫炎 <68401855+Aaron8052@users.noreply.github.com>
2024-04-10 10:37:05 -0400

    Update Loop2.asm

--- 1400/Loop2.asm
+++ 1400/Loop2.asm
@@ -3,6 +3,7 @@
 #Do input validation for the user input
 .data
 Prompt:		.asciiz "Please enter a positive integer: "
+Error:		.asciiz "Not accept negative or zero\n"
 Line:		.asciiz "\nLoop:"
 Sum:		.asciiz "\nThe sum is "
 .text
@@ -14,7 +15,7 @@ main:
 	
 	li	$v0, 5		#input
 	syscall
-	move	$t0, $v0	#t1 = input
+	move	$t0, $v0	#t0 = input
 	
 	#input validation
 	#get input
@@ -23,29 +24,38 @@ main:
 	#	get input 
 	#end loop
 	
-	bltz	$t1, invalidInput
+	bgtz	$t0, validInput
+	
 invalidInput:
 	#need complete the code
 	#
-
-	li	$t1, 0		#t2 = 0
-	li	$s0, 0		#s0 = 0
-loop:
-	addi	$t1, $t1, 1	#t2++
-	add	$s0, $s0, $t1	#s0 += t2
+	li	$v0, 4
+	la	$a0, Error
+	syscall
 	
-	li	$v0, 4		#print "\nLoop:"
+	b 	main
+	
+validInput:
+	li	$t1, 0			#t1 = 0
+	li	$s0, 0			#s0 = 0
+loop:
+	bge	$t1, $t0, result	#if(t1 >= t0) result
+	
+	addi	$t1, $t1, 1		#t1++
+	add	$s0, $s0, $t1		#s0 += t1
+	
+	li	$v0, 4			#print "\nLoop:"
 	la	$a0, Line
 	syscall
 	
 	li	$v0, 1
 	move	$a0, $t1
-	syscall			#print t2
+	syscall				#print t1
 	
-	blt	$t1, $t0, loop	#if(t2<=t1) => loop
+	blt	$t1, $t0, loop		#if(t2<=t0) => loop
 	
 result:
-	li	$v0, 4		#print sum
+	li	$v0, 4			#print sum
 	la	$a0, Sum
 	syscall
 	
