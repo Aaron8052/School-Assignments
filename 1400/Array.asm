05efb6b8eee5afd3b44d809b7b2eea5f1882b37d
FengYan <jiangyu8052@gmail.com>
2024-04-11 11:23:13 -0400

    1400 Array IO

--- 1400/Array.asm
+++ 1400/Array.asm
@@ -2,7 +2,7 @@
 #int array[1024]
 array:		.space 4096
 #int pof2[16] = {1, 2, 4, 8,16,32, 64,128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768}
-pof2:		.word 1, 2, 4, 8,16,32, 64,128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768
+pof2:		.word 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768
 #char* charArr = "assembly"
 charArr:	.asciiz	"assembly"	#8 chars string
 charArr2:	.space 9		#one more space for nullterminate
@@ -11,6 +11,8 @@ charArr2:	.space 9		#one more space for nullterminate
 
 
 .text
-
+	#lb	Rs, Add		#load byte
+	
 	la	$a0, pof2	# a0 = $pof2
-	lw	$s0, 8($a0)	# s0 = MEM(a0 + 8)
+	lw	$s0, 8($a0)	# s0 = MEM(a0 + 8) or *(array + 2) 8 bytes = offset 2 | int = 4bytes
+	sw	$s1, 12($a0)	# s1 = Mem[$a0 +12]
