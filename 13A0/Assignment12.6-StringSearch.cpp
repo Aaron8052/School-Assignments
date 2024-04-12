d1ff8000dfc2468961e301a2d896d35c6317c47d
FengYan <jiangyu8052@gmail.com>
2024-04-12 12:16:01 -0400

    12.6

---
+++ 13A0/Assignment12.6-StringSearch.cpp
@@ -0,0 +1,25 @@
+/*
+    Name: Assignment: 12.6: String Search
+    Copyright: 2024
+    Author: Yu Jiang
+    Date: 
+    Description:
+        Book Chapter 12 Programming Challenges #6 String Search
+        Write a program that asks the user for a file name and a string for which to search.
+        The programshould search the file for every occurrence of a specified string.
+        When the string is found, the line that contains it should be displayed.
+        After all the occurrences have been located,
+        the program should report the number of times the string appeared in the file.
+ */
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    
+    return 0;
+}
