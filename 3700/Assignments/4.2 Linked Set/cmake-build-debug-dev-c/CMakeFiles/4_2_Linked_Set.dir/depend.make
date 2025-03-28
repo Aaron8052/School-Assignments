c08ba9d5990bd71b596eaceb88cfc875aac2ed4c
FengYan <fengyandl@icloud.com>
2025-03-28 13:35:05 -0400

    -

---
+++ 3700/Assignments/4.2 Linked Set/cmake-build-debug-dev-c/CMakeFiles/4_2_Linked_Set.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 4_2_Linked_Set.
+# This may be replaced when dependencies are built.
--- 3700/Assignments/4.2 Linked Set/cmake-build-debug/cmake_install.cmake
+++ 3700/Assignments/4.2 Linked Set/cmake-build-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/jiang/AppData/Local/Programs/CLion/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "D:/Program Files (x86)/Embarcadero/Dev-Cpp/TDM-GCC-64/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
